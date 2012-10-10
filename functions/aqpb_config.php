7304c07df57b7431dcb9d5b9c96a390c35241895
sy4mil <sy4mil@gmail.com>
2012-10-10 19:42:57 +0800

    cleanup

--- functions/aqpb_config.php
+++ functions/aqpb_config.php
@@ -7,6 +7,7 @@
  *
  */
 function aq_page_builder_config() {
+
 	$config = array(); //initialise array
 	
 	$config['menu_title'] = __('Page Builder', 'framework');
@@ -23,104 +24,6 @@ function aq_page_builder_config() {
 	//Debugging
 	$config['debug'] = false;
 	
-	// Available blocks
-	$config['available-blocks'] = array(
-		'text' => array(
-			'type' => 'text',
-			'name' => 'Text',
-			'text' => '',
-			'size' => 'span6',
-		),
-		'slogan' => array(
-			'type' => 'slogan',
-			'name' => 'Slogan',
-			'text' => '',
-			'size' => 'span12',
-		),
-		'slider' => array(
-			'type' => 'slider',
-			'name' => 'Slider',
-			'speed' => '',
-			'pause' => '',
-			'size' => 'span12',
-		),
-		'service' => array(
-			'type' => 'service',
-			'name' => 'Service',
-			'icon' => 'default',
-			'text' => '',
-		),
-		'googlemap' => array (
-			'type' => 'googlemap',
-			'name' => 'Google Map',
-			'size' => 'span6',
-			'address' => '',
-			'coordinates' => '',
-			'height' => 270,
-		), //@todo multi marker map
-		'widgets' => array (
-			'type' => 'widgets',
-			'name' => 'Widgets',
-			'size' => 'span6',
-			'sidebar' => '',
-			'columns' => '',
-		),
-		'blog' => array(
-			'type' => 'blog',
-			'name' => 'Blog Posts',
-			'exclude_format' => '',
-			'category' => '',
-			'tags' => '',
-			'excerpt_length' => '',
-		),
-		'portfolio' => array(
-			'type' => 'portfolio',
-			'name' => 'Portfolio',
-			'columns' => 'four',
-			'filter' => 'true', // show/disable filter
-			'types'	=> '',
-		),
-		'featured_portfolio' => array(
-			'type' => 'featured_portfolio',
-			'name' => 'Featured Portfolio',
-			'size' => 'span6',
-			'items' => 6,
-		),
-		'tab' => array(
-			'type' => 'tab',
-			'name' => 'Tabs',
-			'size' => 'span6',
-			'tabs' => array(),
-		),
-		'toggle' => array(
-			'type' => 'toggle',
-			'name' => 'Toggles',
-			'size' => 'span6',
-			'toggles' => array(),
-		),
-		'accordian' => array(
-			'type' => 'accordian',
-			'name' => 'Accordians',
-			'size' => 'span6',
-			'accordians' => array(),
-		),
-		'price_table' => array(
-			'type' => 'price_table',
-			'name' => 'Price Tables',
-			'size' => 'span6',
-			'toggles' => array(),
-		),
-		'clear' => array(
-			'type' => 'clear',
-			'name' => 'Clear',
-			'size' => 'span12',
-		),
-		'column' => array(
-			'type' => 'column',
-			'name' => 'Column',
-			'size' => 'span12',
-		),
-	);
-	
 	return apply_filters('aq-page-builder-config', $config);
+	
 }
