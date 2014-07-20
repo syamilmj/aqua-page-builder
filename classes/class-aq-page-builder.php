6945cf8ec44409ab3dde551cb32987442ee131c1
Zack Ward <zward84@gmail.com>
2014-07-19 22:37:26 -0600

    When cloning, make sure the clone name isn't already taken

--- classes/class-aq-page-builder.php
+++ classes/class-aq-page-builder.php
@@ -374,8 +374,12 @@ if(!class_exists('AQ_Page_Builder')) {
             $post = get_post( $template_id );
             
             //set up template name
-            $template_name = $post->post_title.'_copy';
-            
+			$clone_number = 1;
+			while (!get_page_by_title($post->post_title.'_copy_'.$clone_number, 'OBJECT', 'template' ) ) {
+				$template_name = $post->post_title.'_copy_'.$clone_number;
+				$clone_number++;
+			}
+			
             //create new template only if title don't yet exist
             if( !get_page_by_title( $template_name, 'OBJECT', 'template' ) ) {
                 
