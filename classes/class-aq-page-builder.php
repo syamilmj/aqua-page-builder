ed953d08a0ab4df3ddf36cb01e9041d0a82490be
Zack Ward <zward84@gmail.com>
2014-07-19 23:11:36 -0600

    Make sure clone name is unique

--- classes/class-aq-page-builder.php
+++ classes/class-aq-page-builder.php
@@ -374,12 +374,11 @@ if(!class_exists('AQ_Page_Builder')) {
             $post = get_post( $template_id );
             
             //set up template name
-			$clone_number = 1;
-			$template_name = $post->post_title.'_copy_'.$clone_number;
-			while (!get_page_by_title($template_name, 'OBJECT', 'template' ) ) {
+			$clone_number = 0;
+			do {
 				$clone_number++;
 				$template_name = $post->post_title.'_copy_'.$clone_number;
-			}
+			} while ( isset( get_page_by_title( $template_name, 'OBJECT', 'template' ) ) );
 			
             //create new template only if title don't yet exist
             if( !get_page_by_title( $template_name, 'OBJECT', 'template' ) ) {
