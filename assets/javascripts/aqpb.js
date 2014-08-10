64644be701eebba8a0468a98f6a2023f31fb7581
Sazzad Hussain <wedcoder09@gmail.com>
2014-08-10 12:57:03 +0600

    improve the remove confirm

--- assets/javascripts/aqpb.js
+++ assets/javascripts/aqpb.js
@@ -301,21 +301,23 @@ jQuery(document).ready(function($){
 	
 	/** Delete Block (via "Delete" anchor) **/
 	$(document).on('click', '.block-control-actions a', function() {
-		if (confirm('Are you sure to Remove it')) {
-			$clicked = $(this);
-			$parent = $(this.parentNode.parentNode.parentNode);
+		
+		$clicked = $(this);
+		$parent = $(this.parentNode.parentNode.parentNode);
 			
-			if($clicked.hasClass('delete')) {
+		if($clicked.hasClass('delete')) {
+			if (confirm('Are you sure to Remove it')) {
 				$parent.find('> .block-bar .block-handle').css('background', 'red');
 				$parent.slideUp(function() {
 					$(this).remove();
 					update_block_order();
 					update_block_number();
 				}).fadeOut('fast');
-			} else if($clicked.hasClass('close')) {
-				$parent.find('> .block-bar a.block-edit').click();
 			}
+		} else if($clicked.hasClass('close')) {
+			$parent.find('> .block-bar a.block-edit').click();
 		}
+		
 		return false;
 	});
 	
